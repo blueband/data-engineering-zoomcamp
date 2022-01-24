f5bdf4c8ba84bb5db02a7fe9dffdb1afe8a13a88
Qamarudeen Muhammad <crescentgroup2000@gmail.com>
2022-01-24 16:30:27 +0100

    assignment done

--- week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
+++ week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
@@ -8,22 +8,22 @@ variable "project" {
 
 variable "region" {
   description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
-  default = "europe-west6"
-  type = string
+  default     = "europe-west6"
+  type        = string
 }
 
 variable "storage_class" {
   description = "Storage class type for your bucket. Check official docs for more info."
-  default = "STANDARD"
+  default     = "STANDARD"
 }
 
 variable "BQ_DATASET" {
   description = "BigQuery Dataset that raw data (from GCS) will be written to"
-  type = string
-  default = "trips_data_all"
+  type        = string
+  default     = "trips_data_all"
 }
 
 variable "credentials" {
-  type = string
-  default = ""
+  type    = string
+  default = "dtc-de-course-2022-339213-d1aeacd3c04b.json"
 }
